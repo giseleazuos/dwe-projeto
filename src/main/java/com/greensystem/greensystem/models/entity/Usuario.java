cbed9da5e31caf9103c9d483331b38a65f36c3f3
Gisele <giselesouza001@outlook.com>
2024-11-05 14:09:01 -0300

    Adicionando projeto

---
+++ src/main/java/com/greensystem/greensystem/models/entity/Usuario.java
@@ -0,0 +1,10 @@
+package com.greensystem.greensystem.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="cliente")
+public class Usuario {
+
+}
